99c5fb831b753860a1d0b91e77cd2045341e3aed
cgoulart01 <cgoulart.rj@gmail.com>
2021-07-18 18:49:43 -0300

    exemplo da pilha de execução do Java

--- Pilha/src/Fluxo.java
+++ Pilha/src/Fluxo.java
@@ -1,15 +1,18 @@
-
+//exemplo da pilha de execuções no java
 public class Fluxo {
 
+//O primeiro método executado é o main
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        metodo1();
+        metodo1(); //o main precisa executar o metodo1 para 
+        //finalizar o main
         System.out.println("Fim do main");
     }
 
     private static void metodo1() {
         System.out.println("Ini do metodo1");
-        metodo2();
+        metodo2();//precisar executar o metodo2 para finalizar
+        // o metodo1
         System.out.println("Fim do metodo1");
     }
 
@@ -18,6 +21,9 @@ public class Fluxo {
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
         }
+        //não tem nenhuma chamada de metodo então é finalizado,
+        // e nesse exemplo os métodos em espera terminam a execução
+        // na sequência  fim metodo2 >fim metodo1>fim main
         System.out.println("Fim do metodo2");
     }
 }
