d54b5b68ce0b8c5f1a196a653a3e20ad12fe969c
cgoulart01 <cgoulart.rj@gmail.com>
2021-07-18 22:37:54 -0300

    forçando uma Exception com throw

---
+++ Pilha/src/Exeptions.java
@@ -0,0 +1,29 @@
+
+public class Exeptions {
+
+
+    public static void main(String[] args) {
+        System.out.println("Ini do main");
+        try {
+        metodo1();
+        }catch(ArithmeticException|NullPointerException x) {
+        	String msg = x.getMessage(); 
+            x.printStackTrace();  
+        	System.out.println("problema ("+msg+") resolvido");
+        }
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Ini do metodo1");
+        metodo2();
+        System.out.println("Fim do metodo1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Ini do metodo2");
+        //throw só pode ser usado para as exceptions
+        throw new NullPointerException("Deu Errado"); 
+       
+    }
+}
